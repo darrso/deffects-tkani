83616de002df69476e624a2b66086c8ccca3e466
darrso <lfif1123@yandex.ru>
2023-11-16 00:30:20 +0300

    Add choose photo form

---
+++ InvPhotos/ChoosePhoto.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ChoosePhoto
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SelectPhotoL = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.PictureBox();
+            this.InWindow = new System.Windows.Forms.PictureBox();
+            this.Svernut = new System.Windows.Forms.PictureBox();
+            this.Info = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InWindow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Svernut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Info)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.draganddrop;
+            this.pictureBox1.Location = new System.Drawing.Point(217, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.GetPhoto);
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            // 
+            // SelectPhotoL
+            // 
+            this.SelectPhotoL.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.SelectPhotoL.AutoSize = true;
+            this.SelectPhotoL.BackColor = System.Drawing.Color.Transparent;
+            this.SelectPhotoL.Font = new System.Drawing.Font("Bahnschrift", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectPhotoL.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.SelectPhotoL.Location = new System.Drawing.Point(755, 715);
+            this.SelectPhotoL.Name = "SelectPhotoL";
+            this.SelectPhotoL.Size = new System.Drawing.Size(162, 28);
+            this.SelectPhotoL.TabIndex = 1;
+            this.SelectPhotoL.Text = "Выбрать фото";
+            this.SelectPhotoL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SelectPhotoL.Click += new System.EventHandler(this.GetPhoto);
+            this.SelectPhotoL.MouseEnter += new System.EventHandler(this.SelectPhotoL_MouseEnter);
+            this.SelectPhotoL.MouseLeave += new System.EventHandler(this.SelectPhotoL_MouseLeave);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.richTextBox1.Location = new System.Drawing.Point(923, 248);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(204, 249);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // GoButton
+            // 
+            this.GoButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.GoButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GoButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.GoButton.FlatAppearance.BorderSize = 10;
+            this.GoButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.GoButton.Location = new System.Drawing.Point(923, 503);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(204, 50);
+            this.GoButton.TabIndex = 3;
+            this.GoButton.Text = "Обработка";
+            this.GoButton.UseVisualStyleBackColor = false;
+            this.GoButton.Visible = false;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // Close
+            // 
+            this.Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Close.BackColor = System.Drawing.Color.Transparent;
+            this.Close.Image = global::WindowsFormsApp1.Properties.Resources._640px_Cross_red_circle_svg;
+            this.Close.Location = new System.Drawing.Point(1114, 0);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(20, 20);
+            this.Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Close.TabIndex = 4;
+            this.Close.TabStop = false;
+            this.Close.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // InWindow
+            // 
+            this.InWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.InWindow.BackColor = System.Drawing.Color.Transparent;
+            this.InWindow.Image = global::WindowsFormsApp1.Properties.Resources.copy_document_blue_circle_20582__2_;
+            this.InWindow.Location = new System.Drawing.Point(1092, 0);
+            this.InWindow.Name = "InWindow";
+            this.InWindow.Size = new System.Drawing.Size(20, 20);
+            this.InWindow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.InWindow.TabIndex = 5;
+            this.InWindow.TabStop = false;
+            this.InWindow.Click += new System.EventHandler(this.InWindow_Click);
+            // 
+            // Svernut
+            // 
+            this.Svernut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Svernut.BackColor = System.Drawing.Color.Transparent;
+            this.Svernut.Image = global::WindowsFormsApp1.Properties.Resources._9884852;
+            this.Svernut.Location = new System.Drawing.Point(1069, 0);
+            this.Svernut.Name = "Svernut";
+            this.Svernut.Size = new System.Drawing.Size(20, 20);
+            this.Svernut.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Svernut.TabIndex = 6;
+            this.Svernut.TabStop = false;
+            this.Svernut.Click += new System.EventHandler(this.Svernut_Click);
+            // 
+            // Info
+            // 
+            this.Info.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Info.BackColor = System.Drawing.Color.Transparent;
+            this.Info.Image = global::WindowsFormsApp1.Properties.Resources.information_button_black_circle_outline_18682;
+            this.Info.Location = new System.Drawing.Point(1113, 744);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(20, 20);
+            this.Info.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Info.TabIndex = 7;
+            this.Info.TabStop = false;
+            this.Info.Click += new System.EventHandler(this.Info_Click);
+            // 
+            // ChoosePhoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._1428926_download_hd_wallpaper_black_and_white_1920x1080_for_ipad;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1134, 763);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.Svernut);
+            this.Controls.Add(this.InWindow);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.SelectPhotoL);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ChoosePhoto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Choose Photo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ChoosePhoto_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ChoosePhoto_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ChoosePhoto_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InWindow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Svernut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Info)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label SelectPhotoL;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.PictureBox Close;
+        private System.Windows.Forms.PictureBox InWindow;
+        private System.Windows.Forms.PictureBox Svernut;
+        private System.Windows.Forms.PictureBox Info;
+    }
+}
+
---
+++ InvPhotos/ChoosePhoto.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ChoosePhoto : Form
+    {
+        // move window params
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        // image params
+        private Bitmap image;
+        private string image_name;
+        public ChoosePhoto()
+        {
+            InitializeComponent();
+            Point location_middle = new Point(this.Width / 2, this.Height / 2);
+            openFileDialog1.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            pictureBox1.AllowDrop = true;
+        }
+        #region Select photo by openFileDialog
+        private void GetPhoto(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(openFileDialog1.FileName);
+                image_name = openFileDialog1.FileName;
+                pictureSelected();
+            }
+        }
+
+        private void SelectPhotoL_MouseEnter(object sender, EventArgs e)
+        {
+            SelectPhotoL.ForeColor = Color.SkyBlue;
+        }
+        private void SelectPhotoL_MouseLeave(object sender, EventArgs e)
+        {
+            SelectPhotoL.ForeColor = Color.SteelBlue;
+        }
+        #endregion
+
+        #region Select photo by Drag and Drop
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                string[] fileNames = data as string[];
+                if (fileNames.Length > 0)
+                {
+                    image = (Bitmap)Image.FromFile(fileNames[0]);
+                    image_name = fileNames[0];
+                    pictureSelected();
+                }
+            }
+        }
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+        }
+        #endregion
+
+        private void pictureSelected()
+        {
+            pictureBox1.Image = image;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            setInfoToRichBox();
+            GoButton.Visible = true;
+        }
+        private static (Color, int) GetMostUsedColor(Bitmap theBitMap)
+        {
+            List<Color> TenMostUsedColors = new List<Color>();
+            List<int> TenMostUsedColorIncidences = new List<int>();
+
+            Color MostUsedColor = Color.Empty;
+            int MostUsedColorIncidence = 0;
+
+            Dictionary<int, int> dctColorIncidence = new Dictionary<int, int>();
+
+            // this is what you want to speed up with unmanaged code
+            for (int row = 0; row < theBitMap.Size.Width; row++)
+            {
+                for (int col = 0; col < theBitMap.Size.Height; col++)
+                {
+                    int pixelColor = theBitMap.GetPixel(row, col).ToArgb();
+
+                    if (dctColorIncidence.Keys.Contains(pixelColor))
+                    {
+                        dctColorIncidence[pixelColor]++;
+                    }
+                    else
+                    {
+                        dctColorIncidence.Add(pixelColor, 1);
+                    }
+                }
+            }
+            var dctSortedByValueHighToLow = dctColorIncidence.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (KeyValuePair<int, int> kvp in dctSortedByValueHighToLow.Take(10))
+            {
+                TenMostUsedColors.Add(Color.FromArgb(kvp.Key));
+                TenMostUsedColorIncidences.Add(kvp.Value);
+            }
+
+            return (Color.FromArgb(dctSortedByValueHighToLow.First().Key), dctSortedByValueHighToLow.First().Value);
+        }
+
+        private void setInfoToRichBox()
+        {
+            int image_width = ((Image)image).Width;
+            int image_height = ((Image)image).Height;
+            Color c;
+            int i;
+            (c, i)= GetMostUsedColor(image);
+
+            string text = $"Size: {image_width}x{image_height}\n" +
+                $"Most used color: {c.ToString()}\n"+
+                $"Image path: {image_name}";
+            richTextBox1.Text = text;
+            richTextBox1.Visible = true;
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Window
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Вы уверены, что хотите закрыть программу?", "Закрытие", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            
+        }
+
+        private void InWindow_Click(object sender, EventArgs e)
+        {
+            if(WindowState== FormWindowState.Maximized)
+            {
+                WindowState= FormWindowState.Normal;
+            }
+            else
+            {
+                WindowState= FormWindowState.Maximized;
+            }
+            
+        }
+
+        private void Svernut_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void ChoosePhoto_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void ChoosePhoto_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void ChoosePhoto_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+        #endregion
+
+        private void Info_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog();
+        }
+    }
+}
